7bd67f009d3e04861f7ece0f4635493aac985240
Ayrton Marcazzo <ayrtonmarcazzo@gmail.com>
2019-04-19 15:26:59 -0300

    Updates

---
+++ precache-manifest.145b95240624f2afbdc11627ecb9a074.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "98c099ff63efd8d16886",
+    "url": "/aerolab-challenge/static/css/main.aff91608.chunk.css"
+  },
+  {
+    "revision": "98c099ff63efd8d16886",
+    "url": "/aerolab-challenge/static/js/main.65eca5bd.chunk.js"
+  },
+  {
+    "revision": "ec27a2cda7987cd3902b",
+    "url": "/aerolab-challenge/static/js/runtime~main.a572dc51.js"
+  },
+  {
+    "revision": "1c35352d1ef63c9f6388",
+    "url": "/aerolab-challenge/static/js/2.2f7bd573.chunk.js"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/aerolab-challenge/static/media/header-x1.f0b98264.png"
+  },
+  {
+    "revision": "bedef3832219792f30be7f676dffa61a",
+    "url": "/aerolab-challenge/static/media/arrow-right.bedef383.svg"
+  },
+  {
+    "revision": "2e6efe58997cf9953ce94c4829d49f8e",
+    "url": "/aerolab-challenge/static/media/arrow-left.2e6efe58.svg"
+  },
+  {
+    "revision": "71af6f4be21b71f7b37a986aca3a5af6",
+    "url": "/aerolab-challenge/static/media/aerolab-logo.71af6f4b.svg"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/aerolab-challenge/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/aerolab-challenge/static/media/buy-blue.2f11a957.svg"
+  },
+  {
+    "revision": "40bc281346af2ba434a9d9792491715d",
+    "url": "/aerolab-challenge/static/media/buy-white.40bc2813.svg"
+  },
+  {
+    "revision": "4245a658668ad469e13bd67b2f99b90e",
+    "url": "/aerolab-challenge/index.html"
+  }
+];
